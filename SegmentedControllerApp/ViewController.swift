9761a98d54bee315eff5d5bb336225af0e00c0e3
Ava Schmidt <ava.schmidt25@d155.org>
2024-09-26 19:09:40 -0700

    Notes complete

--- SegmentedControllerApp/ViewController.swift
+++ SegmentedControllerApp/ViewController.swift
@@ -1,19 +1,31 @@
-//
-//  ViewController.swift
-//  SegmentedControllerApp
-//
-//  Created by AVA SCHMIDT on 9/26/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        var select = sender.selectedSegmentIndex
+//        if select == 0{
+//            print("You selected pony")
+//        }
+//        else if select == 1{
+//            print("You selected dog")
+//        }
+        switch select {
+        case 0:
+            print("You selected pony")
+        case 1:
+            print("You selected dog")
+        case 2:
+            print("You selected bear")
+        default:
+            print("You didn't select anything")
+        }
+    }
+    
 }
 
