a35bef1aee2a21f8cfa22202d97b8b7760145bd3
Ava Schmidt <ava.schmidt25@d155.org>
2024-09-26 18:51:12 -0700

    Initial Commit

---
+++ SegmentedControllerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedControllerApp
+//
+//  Created by AVA SCHMIDT on 9/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SegmentedControllerAppTests/SegmentedControllerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControllerAppTests.swift
+//  SegmentedControllerAppTests
+//
+//  Created by AVA SCHMIDT on 9/26/24.
+//
+
+import Testing
+@testable import SegmentedControllerApp
+
+struct SegmentedControllerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
